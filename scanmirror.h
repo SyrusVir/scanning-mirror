d99e2e3bbd7e2d0cafeb34233ed17a8149f96a0f
SyrusVir <jude.alnas@gmail.com>
2021-01-29 00:56:08 -0600

    Implemented scanning mirror control module

---
+++ scanmirror.h
@@ -0,0 +1,14 @@
+#include <pigpio.h>
+#include <stdint.h>
+
+#define MIRROR_FULL_DUTY 1000000
+typedef struct Mirror {
+    uint8_t FREQ_PIN;
+    uint8_t ENABLE_PIN;
+    uint8_t ATSPEED_PIN;
+} mirror_t;
+
+uint8_t enableMirror(mirror_t);
+uint8_t disableMirror(mirror_t);
+uint8_t setMirrorRPM(mirror_t, uint16_t);
+uint8_t checkMirrorAtSpeed(mirror_t);
