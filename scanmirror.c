37f88a75b13bce40eb02dfcf817ad09a9b4e4193
SyrusVir <jude.alnas@gmail.com>
2021-02-04 15:28:10 -0600

    Error propagation

--- scanmirror.c
+++ scanmirror.c
@@ -1,21 +1,47 @@
 #include <scanmirror.h>
 #include <stdio.h>
 
-uint8_t configMirror(mirror_t m) {
-    gpioSetMode(m.ENABLE_PIN, PI_OUTPUT);
-    gpioSetMode(m.ATSPEED_PIN, PI_INPUT);
-    gpioSetMode(m.FREQ_PIN, PI_ALT5);
+unsigned int const MIRROR_FULL_DUTY = 1000000;
+
+int configMirror(mirror_t m) {
+    char err_msg[] = "configMirror: PIGPIO EXCEPTION %d WITH %s\n";
+    int status;
+    status = gpioSetMode(m.ENABLE_PIN, PI_OUTPUT);
+    if (status < 0) {
+        printf(err_msg, status, "gpioSetMode(m.ENABLE_PIN)");
+        return status;
+    }
+
+    status = gpioSetMode(m.ATSPEED_PIN, PI_INPUT);
+    if (status < 0) {
+        printf(err_msg, status, "gpioSetMode(m.ATSPEED_PIN)");
+        return status;
+    }
+    
+    status = gpioSetPullUpDown(m.ATSPEED_PIN, PI_PUD_UP);
+    if (status < 0) {
+        printf(err_msg, status, "gpioSetPullUpDown(m.ATSPEED_PIN, PI_PUD_UP)");
+        return status;
+    }
+
+    status = gpioSetMode(m.FREQ_PIN, PI_ALT5);
+    if (status < 0) {
+        printf(err_msg, status, "gpioSetMode(m.FREQ_PIN, PI_ALT5)");
+        return status;
+    }
+
+    return 0;
 }
 
-uint8_t enableMirror(mirror_t m) {
+int enableMirror(mirror_t m) {
     return gpioWrite(m.ENABLE_PIN,0);
 }
 
-uint8_t diableMirror(mirror_t m) {
+int diableMirror(mirror_t m) {
     return gpioWrite(m.ENABLE_PIN,1);
 }
 
-uint8_t setMirrorRPM(mirror_t m, uint16_t rpm) {
+int setMirrorRPM(mirror_t m, uint16_t rpm) {
     if (rpm > 4000) {
         printf("%u RPM provided. GECKO-FOUR mirror recommends RPM's in range 1000 to 4000.\n", rpm);
         rpm = 4000;
@@ -25,7 +51,12 @@ uint8_t setMirrorRPM(mirror_t m, uint16_t rpm) {
     return gpioHardwarePWM(m.FREQ_PIN,freq,(unsigned)MIRROR_FULL_DUTY/2);
 }
 
-uint8_t checkMirrorAtSpeed(mirror_t m) {
+int checkMirrorAtSpeed(mirror_t m) {
     /**Returns 1 if mirror is at desired speed and 0 if not. **/
-    return 1 - gpioRead(m.ATSPEED_PIN); //atspeed pin is LOW when at speed
+    int status;
+    status = gpioRead(m.ATSPEED_PIN);
+    if (status >= 0) {
+        return (1 - gpioRead(m.ATSPEED_PIN)); //atspeed pin is LOW when at speed
+    }
+    else return status;
 }
--- scanmirror.h
+++ scanmirror.h
@@ -1,14 +1,14 @@
 #include <pigpio.h>
 #include <stdint.h>
 
-unsigned int const MIRROR_FULL_DUTY = 1000000;
 typedef struct Mirror {
     uint8_t FREQ_PIN;
-    uint8_t ENABLE_PIN;
-    uint8_t ATSPEED_PIN;
+    int ENABLE_PIN;
+    int ATSPEED_PIN;
 } mirror_t;
 
-uint8_t enableMirror(mirror_t);
-uint8_t disableMirror(mirror_t);
-uint8_t setMirrorRPM(mirror_t, uint16_t);
-uint8_t checkMirrorAtSpeed(mirror_t);
+int configMirror(mirror_t mirror);
+int enableMirror(mirror_t mirror);
+int disableMirror(mirror_t mirror);
+int setMirrorRPM(mirror_t mirror, uint16_t rpm);
+int checkMirrorAtSpeed(mirror_t mirror);
