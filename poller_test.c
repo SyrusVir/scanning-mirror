7d5238d1f0e84d5bbabf636b81637efa1ebea1df
SyrusVir <jude.alnas@gmail.com>
2021-03-15 00:38:14 -0500

    poller test code working; tests ongoing

--- poller_test.c
+++ poller_test.c
@@ -1,20 +1,37 @@
+#define _GNU_SOURCE
 #include <pigpio.h>
 #include <stdio.h>
 #include <stdlib.h> 
 #include <ncurses.h>
 #include "sos_poller.c"
+#include <sched.h>
 
 #define THREAD_IN 23        //physical pin 16; pin that the threaded poller will poll
 #define THREAD_OUT 24       //physical pin 18; This pin will be pulled LO when THREAD_IN is polled as LO
 #define THREAD_CONTROL 25   //physical pin 22 output pin wired to THREAD_IN
 
-#define DELAY_USEC 500 //microseconds after THREAD_IN or ALERT_IN is triggered to reset *_OUT pin to HI
+#define DELAY_USEC 300 //microseconds after THREAD_IN or ALERT_IN is triggered to reset *_OUT pin to HI
+
+//Function to simulate mutex operation in main. Pulse GPIO if mutex obtained
+void* threadOutFunc(void* arg_lock)
+{
+    pthread_mutex_t* lock = (pthread_mutex_t*)arg_lock;
+
+    while(1)
+    {
+        if (pthread_mutex_trylock(lock) != 0)
+        {
+            pthread_mutex_unlock(lock);
+            gpioTrigger(THREAD_OUT,5,1);
+        }
+    }
+}
 
 int main() 
 {
     //ncurses init
     initscr();
-    nodelay(stdscr,FALSE);
+    nodelay(stdscr,TRUE);
     noecho();
 
     //Pigpiio intialization
@@ -28,22 +45,70 @@ int main()
     
     //initialize output pin;
     gpioWrite(THREAD_CONTROL, 1);
-    gpioWrite(THREAD_OUT,1);
+    gpioWrite(THREAD_OUT,0);
 
     //initialize poller
     pthread_t poller_thread_id;
-    sos_poller_t* poller = sosPollerInit(THREAD_IN, 0);
-
-    pthread_create(&poller_thread_id, NULL, &sosPoller, poller);
-
-    while(getch() != 'q')
-    {
-        printf("looping...");
-    } 
+    pthread_t pulse_thread_id;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
     
+    pthread_mutex_t lock;
+    pthread_mutex_init(&lock, NULL);
+
+    sos_poller_t* poller = sosPollerInit(&lock, THREAD_IN, 0, DELAY_USEC);
+
+    //set thread affinities
+    cpu_set_t cpu_mask;
+    
+    CPU_ZERO(&cpu_mask);
+    CPU_SET(1, &cpu_mask);
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_mask), &cpu_mask);
+    printf("here\n\r");
+    pthread_create(&poller_thread_id, &attr, &sosPoller, poller);
+
+    CPU_ZERO(&cpu_mask);
+    CPU_SET(2, &cpu_mask);
+    if (pthread_attr_setaffinity_np(&attr,sizeof(cpu_mask), &cpu_mask) != 0)
+    {
+        perror("pulse");
+    }
+    pthread_create(&pulse_thread_id, &attr, &threadOutFunc, &lock);
+
+    CPU_ZERO(&cpu_mask);
+    CPU_SET(0, &cpu_mask);
+    if(pthread_setaffinity_np(pthread_self(),sizeof(cpu_mask), &cpu_mask) != 0)
+    {
+        perror("main");
+    }
+
+
+
+    int c = getch();
+    while(c != 'q')
+    {
+        c = getch();
+        if (c != ERR && c != 'q') 
+        {
+            printf("trigger_tick=%ld\n\r",gpioTick());
+            gpioTrigger(THREAD_CONTROL,2,0);
+        }
+        
+        /* if(pthread_mutex_trylock(&poller->laser_mutex) == 0)
+        {
+            printf("locked...\n\r");
+            pthread_mutex_unlock(&poller->laser_mutex);
+        } 
+        else printf("lock failed...\n\r"); */
+    } 
+
+    printf("exiting\n\r");
     poller->exit_flag = 1;
     pthread_join(poller_thread_id, NULL);
+    pthread_cancel(pulse_thread_id);
+    sosPollerDestroy(poller);
+    pthread_mutex_destroy(&lock);
     gpioTerminate();
-    echo();
+    nodelay(stdscr, FALSE);
     endwin();
 }
--- sos_poller.c
+++ sos_poller.c
@@ -5,26 +5,28 @@
 
 typedef struct SOSPoller
 {
+    pthread_mutex_t* laser_mutex;    //mutex to lock to indicate [trigger_level] was detected at pin [sos_detector_pin]
+    pthread_spinlock_t* spin;    //mutex to lock to indicate [trigger_level] was detected at pin [sos_detector_pin]
     uint8_t exit_flag;  //raise flag to exit poller and poller thread
     uint8_t trigger_level;  //1 or 0; GPIO level poller looks for
     uint8_t sos_detector_pin;   //RPi pin to poll for [trigger_level]
-    pthread_mutex_t laser_mutex;    //mutex to lock to indicate [trigger_level] was detected at pin [sos_detector_pin]
     uint32_t delay_usec;    // after delay_usec, laser_mutex is unlocked 
 } sos_poller_t;
 
-sos_poller_t* sosPollerInit(int sos_detector_pin, int trigger_level)
+sos_poller_t* sosPollerInit(pthread_mutex_t* lock, int sos_detector_pin, int trigger_level, uint32_t delay_usec)
 {
     sos_poller_t* poller = malloc(sizeof(sos_poller_t));
     poller->sos_detector_pin = sos_detector_pin;
     poller->exit_flag = 0;
+    poller->delay_usec = delay_usec;
     poller->trigger_level = (trigger_level ? 1 : 0); //return 1 if [trigger_level] non_zero, 0 otherwise
-    pthread_mutex_init(&poller->laser_mutex,NULL);
+    poller->laser_mutex = lock;
     return poller;
 } //end sosPollerInit()
 
 int sosPollerDestroy(sos_poller_t* poller)
 {
-    if (pthread_mutex_destroy(&poller->laser_mutex) == EBUSY) return EBUSY; //return with error if attempting to destroy a mutex locked elsewhere
+    if (pthread_mutex_destroy(poller->laser_mutex) == EBUSY) return EBUSY; //return with error if attempting to destroy a mutex locked elsewhere
     free(poller);
 
     return 0;
@@ -38,15 +40,22 @@ void* sosPoller(void* sos_poller_arg)
     uint8_t pin = poller->sos_detector_pin;
     uint8_t trigger_level = poller->trigger_level; 
     uint32_t delay_usec = poller->delay_usec;
-    pthread_mutex_t* lock = &poller->laser_mutex; 
-
+    pthread_mutex_t* lock = poller->laser_mutex; 
+    
     //main polling loop
     while(!poller->exit_flag)
     {
-        while (gpioRead(pin) != trigger_level) {} //poll until [trigger_level]
-        pthread_mutex_lock(lock);   //lock mutex, stopping emissioon
-        gpioDelay(delay_usec);              //delay for specified microseconds
-        pthread_mutex_unlock(lock); //unlock mutex, allowing emission
+        //poll until [trigger_level] or exit_flag. 
+        //gpioRead first to take advantage of short-circuit conditional evaluation
+        printf("before poll\n\r");
+        while (gpioRead(pin) != trigger_level && !poller->exit_flag); 
+        printf("after poll\n\r");
+        uint32_t poller_tick = gpioTick();
+        if (poller->exit_flag) break;   //exit loop if exit_flag set
+        pthread_mutex_lock(lock);       //else lock mutex, stopping emission
+        gpioDelay(delay_usec);          //delay for specified microseconds
+        pthread_mutex_unlock(lock);     //unlock mutex, allowing emission
+        printf("poller_tick=%ld\n\r",poller_tick);
     } //end while(!poller->exit_flag)
 
     return NULL;
