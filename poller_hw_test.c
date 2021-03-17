5160b4575e0f7c233e701d7250c1ca3dc09cd9b2
SyrusVir <jude.alnas@gmail.com>
2021-03-17 03:17:23 -0500

    Oscope tests all good!

--- poller_hw_test.c
+++ poller_hw_test.c
@@ -9,18 +9,18 @@
 #include "pin_poller.c"
 
 //Mirror and SOS pins
-#define MIRROR_SOS_PIN 24           // physical pin 18
-#define MIRROR_SOS_DELAY_USEC 50    // usecs to delay once SOS signal detected by poller
-#define MIRROR_SPEED_PIN 23         // physical pin 16
-#define MIRROR_ENABLE_PIN 5         // physical pin 29
-#define MIRROR_PWM_PIN 18           // physical pin 12
+#define MIRROR_PWM_PIN 18           // physical pin 12; mirror control PWM output
+#define MIRROR_SPEED_PIN 23         // physical pin 16; mirror ATSPEED input
+#define MIRROR_SOS_PIN 24           // physical pin 18; sos detector input
+#define MIRROR_ENABLE_PIN 25        // physical pin 22; mirror ENABLE output
+#define MIRROR_SOS_DELAY_USEC 100    // usecs to delay once SOS signal detected by poller
 
 //laser pins
 #define LASER_ENABLE_PIN 5      // physical pin 29
 #define LASER_SHUTTER_PIN 6     // physical pin 31
 #define LASER_PULSE_PIN 13       // physical pin 33
 #define LASER_PULSE_FREQ 1000       
-#define LASER_PULSE_WIDTH_USEC 3
+#define LASER_PULSE_WIDTH_USEC 10
 #define LASER_PULSE_DUTY (unsigned int)(LASER_PULSE_WIDTH_USEC / 1000000.0 * LASER_PULSE_FREQ * PI_HW_PWM_RANGE)
 
 typedef enum state
@@ -40,8 +40,7 @@ void enableLaser()
     gpioWrite(LASER_ENABLE_PIN,0);
 
     gpioSetMode(LASER_PULSE_PIN, PI_OUTPUT);
-    gpioWrite(LASER_PULSE_PIN,0);
-
+    gpioHardwarePWM(LASER_PULSE_PIN, LASER_PULSE_FREQ, LASER_PULSE_DUTY);
     return;
 }
 
@@ -50,6 +49,7 @@ void disableLaser()
     gpioWrite(LASER_SHUTTER_PIN,0);
     gpioWrite(LASER_ENABLE_PIN,0);
     gpioHardwarePWM(LASER_PULSE_PIN, 0, 0);
+
 }
 
 int main() 
@@ -57,6 +57,11 @@ int main()
     // Pigpio Initialize
     gpioCfgClock(1,1,0);    // 1 us sample rate; using PCM clock for hardware PWM
     gpioInitialise();
+    
+    //ncurses initialization
+    initscr();
+    nodelay(stdscr, true);
+    noecho();
 
     //laser pin configuration/////////////////////////////////
     gpioSetMode(LASER_SHUTTER_PIN,PI_OUTPUT);
@@ -98,13 +103,13 @@ int main()
 
     test_state_t state = WAIT;
     
+    int c;
     while (state != STOP)
     {
         switch (state)
         {
             case WAIT:
                 printf("\'q\' to quit.\n\r\'S\' to start.\n\r");
-                int c;
                 do
                 {
                     c = getch();
@@ -120,28 +125,37 @@ int main()
                 //start with mirror startup
                 mirrorEnable(mirror);       //enable mirror
                 mirrorSetRPM(mirror, 1000); //start rotation
+
+                printf("Waiting for mirror at speed...\n\r");
                 while (!mirrorCheckAtSpeed(mirror));    //wait until at speed
                 
                 //Do not start laser until after first occurance of SOS detection
+                printf("Waiting for poller event...\n\r");
                 while (pinPollerCheckIn(sos_poller) == 0); //wait until first SOS detection
+                printf("Waiting for first lock...\n\r");
                 while (pinPollerCheckIn(sos_poller) != EBUSY); //wait until lock was found available
+                printf("STARTING...\n\r");
                 gpioWrite(LASER_ENABLE_PIN, 1); //start laser emission
-
-                int c = getch();
+                
+                c = getch();
                 while (c != 'q')
                 {
                     //wait until poller releases lock
-                    while (pinPollerCheckIn(sos_poller) != EBUSY)
+                    while (pinPollerCheckIn(sos_poller) !=  EBUSY)
                     {  
-                        gpioWrite(LASER_ENABLE_PIN, 0);
+                        //While the poller lock is free, keep laser enabled
+                        gpioWrite(LASER_ENABLE_PIN, 1);
                         c = getch();
                         if (c == 'q') break;
-                    } //wait until lock was found available
-                    
+                    }
+
+                    //exit if 'q' input
                     if (c == 'q') break;
 
-                    gpioWrite(LASER_ENABLE_PIN,1);
+                    //else disable laser
+                    gpioWrite(LASER_ENABLE_PIN,0);
                 }
+                printf("STOPPING\r");
                 state = STOP;
                 break;
             default:
@@ -162,5 +176,7 @@ int main()
     pthread_spin_destroy(&sos_spinlock);
 
     gpioTerminate();
+    nodelay(stdscr, false);
+    endwin();
     return 0;
 } //end main
